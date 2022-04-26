d8d3eb4204c6301c034b5ab91a21be1b18a40b9f
Julien <mmos002@gmail.com>
2022-04-26 15:43:55 +0200

    svgs are boring.

--- src/components/App.js
+++ src/components/App.js
@@ -12,33 +12,37 @@ import Home from "./Home";
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-          <Router>
-            <AuthProvider>
-            <div className="w-100" style={{ maxWidth: "400px" }}>
-              <Routes>
-                  <Route exact path="/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
-                  <Route path="/signup" element={<Signup />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/forgot-password" element={<ForgotPassword />} />
-                  <Route path="/update-profile" element={<PrivateRoute><UpdateProfile/></PrivateRoute>} />
-              </Routes>
-            </div>
-            </AuthProvider>
-          </Router>
-    </Container>,
-    <Container className="w-100 m-0 p-0" style={{ maxWidth:'100%', minHeight:'auto'}}>
-      <Router>
-        <AuthProvider>
-            <div>
-              <Routes>
-                <Route path="/home" element={<Home/>} />
-                <Route path="/" element={<Navigate to="/home" />} />
-              </Routes>
-            </div>
-        </AuthProvider>
-      </Router>
-    </Container>
+    <>
+      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+            <Router>
+              <AuthProvider>
+              <div className="w-100" style={{ maxWidth: "400px" }}>
+                <Routes>
+                    <Route exact path="/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path="/update-profile" element={<PrivateRoute><UpdateProfile/></PrivateRoute>} />
+                </Routes>
+              </div>
+              </AuthProvider>
+            </Router>
+      </Container>
+    </>,
+    <>
+      <Container className="w-100 m-0 p-0" style={{ maxWidth:'100%', minHeight:'auto'}}>
+        <Router>
+          <AuthProvider>
+              <div>
+                <Routes>
+                  <Route path="/home" element={<Home/>} />
+                  <Route path="/" element={<Navigate to="/home" />} />
+                </Routes>
+              </div>
+          </AuthProvider>
+        </Router>
+      </Container>
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import { ReactComponent as Logo } from './../images/Logo-blanc-H.svg'
 function Header() {
 
   return (
-        <nav className="navbar navbar-expand-xl navbar-dark bg-dark m-0 p-0 w-100 sticky-top" style={{ maxWidth: '100%'}}>
+        <nav className="navbar navbar-expand-xl navbar-dark m-0 p-0 w-100 sticky-top" style={{ maxWidth: '100%', backgroundColor:'#00675B', minHeight:'100px'}}>
             <a className="navbar-brand" href="# ">
-                <Logo style={{ width:'150px', height:'150px'}}/>
+                <Logo style={{maxWidth:'270px', maxHeight:'80px', marginLeft:'90px'}}/>
             </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -29,7 +29,7 @@ function Header() {
                                 <a className="nav-link " href="# ">Contact</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled" href=" #" tabIndex="-1" aria-disabled="true">Disabled</a>
+                                <a className="nav-link disabled" href=" #" tabIndex="-1" aria-disabled="true">x</a>
                             </li>
                         </ul>
                         <a className="navbar-icon" href=" #">

4c5b07c8fbffd6a8800477d330ecec186d3c61bd
Julien <mmos002@gmail.com>
2022-04-27 15:08:07 +0200

    Working on Modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,48 @@
 import { ReactComponent as Logo } from './../images/Logo-blanc-H.svg';
 import { ReactComponent as UserIcon } from './../images/person-circle.svg'
+import ModalAuth from './ModalAuth';
 
 function Header() {
 
-  return (
-      <div className="w-100 m-0 p-0" style={{ maxWidth:'100%', minHeight:'auto'}}>
-        <nav className="navbar navbar-expand-xl sticky-top navbar-dark " style={{ minWidth: '100%', backgroundColor:'#00675B', minHeight:'100px', maxWidth:'100%'}}>
-            <a className="navbar-brand" href="# ">
-                <Logo style={{maxWidth:'270px', maxHeight:'80px', marginLeft:'90px'}}/>
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-                <div className="collapse navbar-collapse">
-                    <div className="navbar d-flex ms-auto me-5">
-                        <ul className="navbar-nav mt-2 mt-lg-0">
-                            <li className="nav-item active">
-                                <a className="nav-link text-white" href="# ">Accueil</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link text-white" href="# ">Présentation</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link text-white" href="# ">Nos prestations</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link text-white" href="# ">Nos Produits</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link text-white" href="# ">Contact</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href=" #" tabIndex="-1" aria-disabled="true">x</a>
-                            </li>
-                        </ul>
-                        <a className="navbar-icon" href=" #">
-                            <UserIcon style={{color:'white', width:'20px'}}/>
-                        </a>
+    return (
+        <div className="w-100 m-0 p-0" style={{ maxWidth:'100%', minHeight:'auto'}}>
+            <nav className="navbar navbar-expand-xl sticky-top navbar-dark " style={{ minWidth: '100%', backgroundColor:'#00675B', minHeight:'100px', maxWidth:'100%'}}>
+                <a className="navbar-brand" href="# ">
+                    <Logo style={{maxWidth:'270px', maxHeight:'80px', marginLeft:'90px'}}/>
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                    <div className="collapse navbar-collapse">
+                        <div className="navbar d-flex ms-auto me-5">
+                            <ul className="navbar-nav mt-2 mt-lg-0">
+                                <li className="nav-item active">
+                                    <a className="nav-link text-white" href="# ">Accueil</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link text-white" href="# ">Présentation</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link text-white" href="# ">Nos prestations</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link text-white" href="# ">Nos Produits</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link text-white" href="# ">Contact</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" href=" #" tabIndex="-1" aria-disabled="true">x</a>
+                                </li>
+                            </ul>
+                            <button onClick={<ModalAuth/>} className="navbar-icon" href=" #">
+                                <UserIcon style={{color:'white', width:'20px'}}/>
+                            </button>
+                        </div>
                     </div>
-                </div>
-        </nav>
-    </div>
-  )
+            </nav>
+        </div>
+    )
 }
 
 export default Header

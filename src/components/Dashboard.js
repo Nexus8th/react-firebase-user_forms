f007902d85e579b8b630a149db0685bb600fd0ef
Julien <mmos002@gmail.com>
2022-04-28 17:47:36 +0200

    Need to fix Buttons onClick Areas

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import { Button, Card, Alert } from 'react-bootstrap';
 import { useState } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
+import { ReactComponent as UserIcon } from './../images/person-circle.svg'
 
 export default function Dashboard() {
 
@@ -32,25 +33,21 @@ export default function Dashboard() {
     return (
         <>
         <div className="d-flex align-items-center justify-content-center w-100" style={{minHeight:'100vh'}}>
-            <Card style={{ maxWidth: "400px" }}>
+            <Card style={{ maxWidth: "400px", backgroundColor:'#EBEBEB', justifyContent:'center', alignItems:'center', textAlign:'center' }}>
                     <Card.Body>
-                        <h2 className="text-center mb-4">Profil</h2>
+                        <div className="mt-3" style={{color:'#009688'}}><UserIcon style={{width:'130px', height:'150px'}}/>
+                        </div>
+                        <h2 className="text-center mb-4 mt-4" style={{color:'#00675B', fontWeight:'400'}}>Profil</h2>
                         {error && <Alert variant="danger">{error}</Alert>}
-                        <div className="w-100 text-center mt-2">
+                        <div style={{color:'#009688'}} className="w-100 text-center mt-2">
                             <strong>Email: &nbsp;</strong>{currentUser.email}
                         </div>
-                        <div className="w-100 text-center mt-2">
+                        <div style={{color:'#009688'}} className="w-100 text-center mt-2">
                             <strong>Pseudo: &nbsp;</strong>{currentUser.pseudo}
                         </div>
-                        <div className="w-100 text-center mt-2">
-                            <Link to="/update-profile" className="btn btn-primary mt-3">Modifier le profil</Link>
-                        </div>
-                        <div className="w-100 text-center mt-2">
-                            <Button variant="primary" onClick={handleLogout}>Se déconnecter</Button>
-                        </div>
-                        <div className="w-100 text-center mt-2">
-                            <Button variant="danger" onClick={handleDelete}>Supprimer le compte</Button>
-                        </div>
+                        <Button  className="w-100 mt-5 rounded-pill" type="submit" style={{backgroundColor:'#009688', borderColor:'#009688'}}><Link to="/update-profile" style={{textDecoration:'none', color:'white'}}>Modifier le profil</Link></Button>
+                        <Button onClick={handleLogout} className="w-100 mt-3 rounded-pill" type="submit" style={{backgroundColor:'white', borderColor:'#009688'}}><Link to="/login" style={{textDecoration:'none', color:'#009688'}}>Se déconnecter</Link></Button>
+                        <Button onClick={handleDelete} className="w-100 mt-5 rounded-pill" type="submit" style={{backgroundColor:'crimson', borderColor:'crimson'}}><Link to="/update-profile" style={{textDecoration:'none', color:'white'}}>Supprimer le profil</Link></Button>
                     </Card.Body>
             </Card>
         </div>
--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
+import { ReactComponent as UserIcon } from './../images/person-circle.svg'
 
 const UpdateProfile = () => {
 
@@ -43,34 +44,34 @@ const UpdateProfile = () => {
     return (
         <>
         <div className="d-flex align-items-center justify-content-center w-100" style={{minHeight:'100vh'}}>
-            <Card style={{ maxWidth: "400px" }}>
+            <Card style={{ maxWidth: "400px", backgroundColor:'#EBEBEB', justifyContent:'center', alignItems:'center', textAlign:'center' }}>
                     <Card.Body>
-                        <h2 className="text-center mb-4">Modifier le profil</h2>
+                        <div className="mt-3" style={{color:'#009688'}}><UserIcon style={{width:'130px', height:'150px'}}/>
+                        </div>
+                        <h2 className="text-center mb-4 mt-3" style={{color:'#00675B', fontWeight:'400'}}>Modifier le profil</h2>
                         {error && <Alert variant="danger">{error}</Alert>}
                         <Form onSubmit={handleSubmit}>
                         <Form.Group id="username">
-                                <Form.Label>Pseudo</Form.Label>
-                                <Form.Control className="mb-3" type="username" placeholder="Entrez votre pseudo" required ref={usernameRef}/>
+                                <Form.Label></Form.Label>
+                                <Form.Control style={{borderColor:'#009688'}} className="rounded-pill border-2 mt-0 mb-0" type="username" placeholder="Entrez votre pseudo" required ref={usernameRef}/>
                             </Form.Group>
                             <Form.Group id="email">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control className="mb-3" type="email" placeholder="Entrez votre email" required defaultValue={currentUser.email} ref={emailRef}/>
+                                <Form.Label></Form.Label>
+                                <Form.Control style={{borderColor:'#009688'}} className="rounded-pill border-2 mt-0 mb-0" type="email" placeholder="Entrez votre email" required defaultValue={currentUser.email} ref={emailRef}/>
                             </Form.Group>
                             <Form.Group id="password">
-                                <Form.Label>Mot de passe</Form.Label>
-                                <Form.Control className="mb-3" type="password" placeholder="Entrez votre mot de passe" ref={passwordRef}/>
+                                <Form.Label></Form.Label>
+                                <Form.Control style={{borderColor:'#009688'}} className="rounded-pill border-2 mt-0 mb-0" type="password" placeholder="Entrez votre mot de passe" ref={passwordRef}/>
                             </Form.Group>
                             <Form.Group id="password-confirm">
-                                <Form.Label>Confirmer le mot de passe</Form.Label>
-                                <Form.Control className="mb-3" type="password" placeholder="Entrez votre mot de passe" ref={confirmPasswordRef}/>
+                                <Form.Label></Form.Label>
+                                <Form.Control style={{borderColor:'#009688'}} className="rounded-pill border-2 mt-0 mb-5" type="password" placeholder="Entrez votre mot de passe" ref={confirmPasswordRef}/>
                             </Form.Group>
-                            <Button disabled={loading} className="w-100" type="submit">Mettre à jour</Button>
+                            <Button disabled={loading} className="w-100 mt-0 rounded-pill" type="submit" style={{backgroundColor:'#009688', borderColor:'#009688'}}>Mettre à jour</Button>
+                            <Button disabled={loading} className="w-100 mt-3 rounded-pill" type="submit" style={{backgroundColor:'#009688', borderColor:'#009688'}}><Link to="/dashboard" style={{textDecoration:'none', color:'white'}} >Annuler</Link></Button>
                         </Form>
                     </Card.Body>
             </Card>
-        </div>
-        <div className="w-100 text-center mt-2">
-            <Link to="/dashboard">Annuler</Link>
         </div>
         </>
     );
